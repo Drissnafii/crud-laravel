8d753a54d2ec11d184461936a8702115a1786f50
drissnafii <drissnafi3@gmail.com>
2025-03-08 20:55:12 +0000

    Add ProfileFactory and update ProfileSeeder to use factory for profile creation

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -4,9 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB as FacadesDB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\Profile;
 
 class ProfileSeeder extends Seeder
 {
@@ -15,11 +16,6 @@ class ProfileSeeder extends Seeder
      */
     public function run(): void
     {
-        FacadesDB::table('profiles')->insert([
-            'name' => Str::random(17),
-            'email' => Str::random(17) . '@gmail.com',
-            'pass' => Hash::make('passme123'),
-            'bio' => Str::random(244),
-        ]);
+        Profile::factory(10)->create();
     }
 }
